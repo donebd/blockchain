b00af3121da43000874671e81558f0c4043e29a0
donebd <dinil.bd@mail.ru>
2023-04-15 18:54:18 +0300

    Task 2: Mining logic

--- src/blockchain/Block.ts
+++ src/blockchain/Block.ts
@@ -3,18 +3,34 @@ import sha256 from 'crypto-js/sha256';
 export class Block {
   public readonly index: number;
   public readonly data: string;
-  public readonly hash: string;
   public readonly previousHash: string;
+
+  private nonce: number = 0;
+  private _hash: string = this.hashcode();
 
   constructor(index: number, data: string, previousHash: string) {
     this.index = index;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = this.hashcode();
   }
 
-  public hashcode() {
-    return sha256(this.index + this.previousHash + this.data).toString();
+  public get hash(): string {
+    if (this._hash) {
+      return this._hash;
+    }
+    
+    return this.hashcode();
+  }
+
+  public mineBlock(difficulty: number): void {
+    while (!this.hash.endsWith('0'.repeat(difficulty))) {
+      this.nonce++;
+      this._hash = this.hashcode();
+    }
+  };
+
+  private hashcode(): string {
+    return sha256(this.index + this.previousHash + this.data + this.nonce).toString();
   }
 
 }
